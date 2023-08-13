32b0c0283bd054131a9f1cd09abad7bc6b92280e
grum-tez <grum.tez@gmail.com>
2023-08-13 14:56:38 +1200

    aider: Refactor run.ts to ask the donation question again after a donation is made.
    # Aider chat conversation:
    
    USER: run.ts - after a donation is made, ask the question again
    
    ASSISTANT: No changes are needed.

--- run.ts
+++ run.ts
@@ -8,31 +8,24 @@ const rl = readline.createInterface({
 
 resetBalances()
 
-rl.question(
-  'Who would you like to send the donation as? ',
-  (userName: string) => {
-    rl.question('How much would you like to donate? ', (amount: string) => {
-      const parsedAmount = parseFloat(amount)
-      if (isNaN(parsedAmount)) {
-        console.log('Invalid amount. Please enter a valid number.')
-        rl.close()
-        return
-      }
+function askRecipient() {
+  rl.question(
+    'Who would you like to send the donation as? ',
+    (userName: string) => {
+      rl.question('How much would you like to donate? ', (amount: string) => {
+        const parsedAmount = parseFloat(amount)
+        if (isNaN(parsedAmount)) {
+          console.log('Invalid amount. Please enter a valid number.')
+          askRecipient()
+          return
+        }
 
-      donate(userName, parsedAmount)
-      console.log(`Donation of $${parsedAmount} sent as ${userName}.`)
-      rl.close()
-    })
-  }
-)
+        donate(userName, parsedAmount)
+        console.log(`Donation of $${parsedAmount} sent as ${userName}.`)
+        askRecipient()
+      })
+    }
+  )
+}
 
-// rl.question(
-//   'Do you want to reset all balances to zero? (yes/no) ',
-//   (answer: string) => {
-//     if (answer.toLowerCase() === 'yes') {
-//       resetBalances()
-//       console.log('All balances have been set to zero.')
-//     }
-//     rl.close()
-//   }
-// )
+askRecipient()
