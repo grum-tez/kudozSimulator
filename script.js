3084cdd5848166da3f07aa00a133afb72fe88499
grum-tez <grum.tez@gmail.com>
2023-08-13 14:14:38 +1200

    Refactored script.js to use separate functions for PDF and CDF calculations, and updated variable names and formatting.

--- script.js
+++ script.js
@@ -1,73 +1,73 @@
-function F(x) {
-  var C = 1.0000000533900888;
-  var a = 1.0;
-  var b = 24.158852886550765;
-  return -C * Math.pow(1 / (x + a), b) + C;
+function PDF(x) {
+  var C = 1.0000000533900888
+  var a = 1.0
+  var b = 24.158852886550765
+  return (b * C * Math.pow(1 / (x + a), b + 1)) / Math.pow(x + a, 2)
 }
 
-function f(x) {
-  var C = 1.0000000533900888;
-  var a = 1.0;
-  var b = 24.158852886550765;
-  return (b * C * Math.pow(1 / (x + a), b + 1)) / Math.pow(x + a, 2);
+function CDF(x) {
+  var C = 1.0000000533900888
+  var a = 1.0
+  var b = 24.158852886550765
+  return -C * Math.pow(1 / (x + a), b) + C
 }
 
 // Generate x values
-var x = [];
+var x = []
 for (var i = 0; i <= 1; i += 0.01) {
-  x.push(i);
+  x.push(i)
 }
 
-// Generate y values for F(x)
-var yF = x.map(function(x) {
-  return F(x);
-});
+// Generate y values for CDF(x)
+var yF = x.map(function (x) {
+  return CDF(x)
+})
 
-// Generate y values for f(x)
-var yf = x.map(function(x) {
-  return f(x);
-});
+// Generate y values for PDF(x)
+var yf = x.map(function (x) {
+  return PDF(x)
+})
 
-// Create the plot for F(x)
+// Create the plot for CDF(x)
 var traceF = {
   x: x,
   y: yF,
   type: 'scatter',
   name: 'Cumulative Density Function',
-  domain: [0, 1]
-};
+  domain: [0, 1],
+}
 
-// Create the plot for f(x)
+// Create the plot for PDF(x)
 var tracef = {
   x: x,
   y: yf,
   type: 'scatter',
   name: 'Probability Density Function',
-  domain: [0, 1]
-};
+  domain: [0, 1],
+}
 
 var layoutF = {
   title: 'Cumulative Density Function',
   xaxis: {
-    title: 'x'
+    title: 'x',
   },
   yaxis: {
-    title: 'y'
-  }
-};
+    title: 'y',
+  },
+}
 
 var layoutf = {
   title: 'Probability Density Function',
   xaxis: {
-    title: 'x'
+    title: 'x',
   },
   yaxis: {
-    title: 'y'
-  }
-};
+    title: 'y',
+  },
+}
 
-var dataF = [traceF];
-var dataf = [tracef];
+var dataF = [traceF]
+var dataf = [tracef]
 
-Plotly.newPlot('plotF', dataF, layoutF);
-Plotly.newPlot('plotf', dataf, layoutf);
+Plotly.newPlot('plotF', dataF, layoutF)
+Plotly.newPlot('plotf', dataf, layoutf)
