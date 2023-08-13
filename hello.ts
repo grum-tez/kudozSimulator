a3aa9d871a0d4da6762b40d8044ee509f9e34898
grum-tez <grum.tez@gmail.com>
2023-08-13 14:18:04 +1200

    aider: Create a function called "donate" in hello.ts that deducts the amount from the user name passed into the function and adds it to Alice's balance.
    # Aider chat conversation:
    
    USER: in hello.ts, create a function called "donate". It should take a user name and an amount. The amount should be deducted from the user name passed into the function, and added to Alice's balance.
    
    ASSISTANT: No changes are needed.

--- hello.ts
+++ hello.ts
@@ -11,3 +11,28 @@ function f(x) {
   var b = 24.158852886550765;
   return (b * C * Math.pow(1 / (x + a), b + 1)) / Math.pow(x + a, 2);
 }
+
+function donate(userName: string, amount: number) {
+  const userRecords = require('./user_records.json');
+  const users = userRecords.users;
+
+  // Find the user with the given name
+  const user = users.find((user: any) => user.name === userName);
+
+  if (user) {
+    // Deduct the amount from the user's balance
+    user.balance -= amount;
+
+    // Find Alice
+    const alice = users.find((user: any) => user.name === 'Alice');
+
+    if (alice) {
+      // Add the amount to Alice's balance
+      alice.balance += amount;
+    }
+  }
+
+  // Update the user records file
+  const fs = require('fs');
+  fs.writeFileSync('./user_records.json', JSON.stringify(userRecords, null, 2));
+}
