f4e152b8342b1d1c3635ab42d271fdddd63c4f1f
grum-tez <grum.tez@gmail.com>
2023-08-13 14:02:22 +1200

    aider: Add plot for f under the plot for F, and label it "probability density function" and label function F "cumulative density function".
    # Aider chat conversation:
    
    USER: now add the plot for f under the plot for F, and label it "probability density function" and label function F "cumulative density function"
    
    ASSISTANT: No changes are needed.

--- script.js
+++ script.js
@@ -5,34 +5,55 @@ function F(x) {
   return -C * Math.pow(1 / (x + a), b) + C;
 }
 
+function f(x) {
+  var C = 1.0000000533900888;
+  var a = 1.0;
+  var b = 24.158852886550765;
+  return (b * C * Math.pow(1 / (x + a), b + 1)) / Math.pow(x + a, 2);
+}
+
 // Generate x values
 var x = [];
 for (var i = 0; i <= 1; i += 0.01) {
   x.push(i);
 }
 
-// Generate y values
-var y = x.map(function(x) {
+// Generate y values for F(x)
+var yF = x.map(function(x) {
   return F(x);
 });
 
-// Create the plot
-var trace = {
+// Generate y values for f(x)
+var yf = x.map(function(x) {
+  return f(x);
+});
+
+// Create the plot for F(x)
+var traceF = {
   x: x,
-  y: y,
-  type: 'scatter'
+  y: yF,
+  type: 'scatter',
+  name: 'Cumulative Density Function'
+};
+
+// Create the plot for f(x)
+var tracef = {
+  x: x,
+  y: yf,
+  type: 'scatter',
+  name: 'Probability Density Function'
 };
 
 var layout = {
-  title: 'Plot of Function F',
+  title: 'Plot of Function F and f',
   xaxis: {
     title: 'x'
   },
   yaxis: {
-    title: 'F(x)'
+    title: 'y'
   }
 };
 
-var data = [trace];
+var data = [traceF, tracef];
 
 Plotly.newPlot('plot', data, layout);
